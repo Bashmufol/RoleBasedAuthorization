5f2e64fa29267fe122d1892df4b8753a88f82d1e
Bashir Muhammed <bashmufol@gmail.com>
2025-07-11 17:08:30 +0100

    updated

--- src/main/java/com/bash/rolebasecrud/model/Employee.java
+++ src/main/java/com/bash/rolebasecrud/model/Employee.java
@@ -1,5 +1,14 @@
 package com.bash.rolebasecrud.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class Employee {
-
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
---
+++ src/main/java/com/bash/rolebasecrud/model/Role.java
@@ -0,0 +1,4 @@
+package com.bash.rolebasecrud.model;
+
+public class Role {
+}
