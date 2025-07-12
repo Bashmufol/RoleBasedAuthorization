68afb3d9f0d105ee05c154c4a98af66bf36af26c
Bashir Muhammed <bashmufol@gmail.com>
2025-07-12 17:20:47 +0100

    updated

--- src/main/java/com/bash/rolebasecrud/dao/RoleDaoImpl.java
+++ src/main/java/com/bash/rolebasecrud/dao/RoleDaoImpl.java
@@ -1,13 +1,25 @@
 package com.bash.rolebasecrud.dao;
 
 import com.bash.rolebasecrud.model.Role;
+import com.bash.rolebasecrud.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@AllArgsConstructor
 public class RoleDaoImpl implements RoleDao {
+    private EntityManager entityManager;
 
     @Override
     public Role findRoleByName(String theRoleName) {
-        return null;
+        TypedQuery<Role> theQuery = entityManager.createQuery("from Role where userName=:roleName", Role.class);
+        theQuery.setParameter("roleName", theRoleName);
+        User theUser = null;
+        try{
+            theUser = theQuery.getSingleResult();
+        } catch (Exception e){theUser = null;}
+        return theUser;
     }
 }
