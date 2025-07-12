04c3fc82cbda8a8bcca786caab1427bbb07188b5
Bashir Muhammed <bashmufol@gmail.com>
2025-07-12 17:13:33 +0100

    updated

--- src/main/java/com/bash/rolebasecrud/dao/UserDaoImpl.java
+++ src/main/java/com/bash/rolebasecrud/dao/UserDaoImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 public class UserDaoImpl implements UserDao {
     private final EntityManager entityManager;
     @Override
-    public User findByUserName(String userName) {
+    public User findByUserName(String theUserName) {
         TypedQuery<User> theQuery = entityManager.createQuery("from User where userName=:uName and enabled=true", User.class);
         theQuery.setParameter("uName", theUserName);
         User theUser = null;
