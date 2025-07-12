435fe48fb3e422430b40deb3ef5ee992524e0e75
Bashir Muhammed <bashmufol@gmail.com>
2025-07-12 17:15:15 +0100

    updated

---
+++ src/main/java/com/bash/rolebasecrud/dao/RoleDaoImpl.java
@@ -0,0 +1,13 @@
+package com.bash.rolebasecrud.dao;
+
+import com.bash.rolebasecrud.model.Role;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl implements RoleDao {
+
+    @Override
+    public Role findRoleByName(String theRoleName) {
+        return null;
+    }
+}
