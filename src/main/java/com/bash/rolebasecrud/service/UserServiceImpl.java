1d3a994ec479d85e19585ceb3b7abcb82a6ac742
Bashir Muhammed <bashmufol@gmail.com>
2025-07-15 07:49:22 +0100

    updated

--- src/main/java/com/bash/rolebasecrud/service/UserServiceImpl.java
+++ src/main/java/com/bash/rolebasecrud/service/UserServiceImpl.java
@@ -1,4 +1,30 @@
 package com.bash.rolebasecrud.service;
 
-public class UserServiceImpl {
+import com.bash.rolebasecrud.dao.RoleDao;
+import com.bash.rolebasecrud.dao.UserDao;
+import com.bash.rolebasecrud.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements  UserService {
+    private UserDao userDao;
+    private RoleDao roleDao;
+
+    @Override
+    public User findByUserName(String userName) {
+        return userDao.findByUserName(userName);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return
+    }
 }
